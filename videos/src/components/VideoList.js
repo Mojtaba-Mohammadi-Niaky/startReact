da8ce42c30fd06dbb82651ea4fbed3f8333f3c81
Mojtaba-Mohammadi-Niaky <MMN0025hc@gmail.com>
2024-07-31 16:28:08 +0330

    React 5th day

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,13 +1,12 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos }) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem />
-    })
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+  });
 
-
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
