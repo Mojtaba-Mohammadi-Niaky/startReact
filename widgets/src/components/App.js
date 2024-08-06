33e0db5571ab83fc9c00e46d000a6e2947fb784d
Mojtaba-Mohammadi-Niaky <MMN0025hc@gmail.com>
2024-08-06 15:31:59 +0330

    React 8th day

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./Accordion";
-import Search from './Search';
+// import Search from './Search';
+import DropDown from "./DropDown";
 
 // const items = [
 //     {
@@ -18,11 +19,40 @@ import Search from './Search';
 //     }
 // ]
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade Of Blue",
+    value: "blue",
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropDown, setShowDropDown] = useState(true);
+
   return (
     <div>
       {/* <Accordion items={items}/> */}
-      <Search />
+      {/* <Search /> */}
+
+      <button onClick={() => setShowDropDown(!showDropDown)}>
+        Toggle Dropdown
+      </button>
+      {showDropDown ? (
+        <DropDown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
