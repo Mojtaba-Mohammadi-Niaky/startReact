d280c0344df86e7d0f9713e03cfac576437c2a61
Mojtaba-Mohammadi-Niaky <MMN0025hc@gmail.com>
2024-08-10 15:05:08 +0330

    React 9th day

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,24 +1,26 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from "react";
-// import Accordion from "./Accordion";
-// import Search from './Search';
-// import DropDown from "./DropDown";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import DropDown from "./DropDown";
 import Translate from "./Translate";
+import Route from "./Route";
+import Header from "./Header";
 
-// const items = [
-//     {
-//         title:'Whay is React?',
-//         content:'React is front end javascript framework'
-//     },
-//     {
-//         title:'Why use React?',
-//         content:'React is a favorite JS library among engineers'
-//     },
-//     {
-//         title:'How do you use React',
-//         content:'You use React by creating components'
-//     }
-// ]
+const items = [
+  {
+    title: "Whay is React?",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
   {
@@ -35,28 +37,28 @@ const options = [
   },
 ];
 
+
 export default () => {
-  // const [selected, setSelected] = useState(options[0]);
-  // const [showDropDown, setShowDropDown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
-    <div> 
-      {/* <Accordion items={items}/> */}
-      {/* <Search /> */}
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
 
-      {/* <button onClick={() => setShowDropDown(!showDropDown)}>
-        Toggle Dropdown
-      </button>
-      {showDropDown ? (
-        <DropDown
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      ) : null} */}
+      <Route path="/list">
+        <Search />
+      </Route>
 
+      <Route path="/dropedown">
+        <DropDown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected}/>
+      </Route>
 
-      <Translate />
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
